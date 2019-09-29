ccf01f4fde70c9467a460a91acd02f6c581cbbd5
Cyril Cesco <cyril.cesco@gmail.com>
2019-09-29 19:41:40 +0200

    Création du workspace et création application home

---
+++ projects/home/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ContactComponent } from './contact/contact.component';
+import { AccueilComponent } from './accueil/accueil.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    ContactComponent,
+    AccueilComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
